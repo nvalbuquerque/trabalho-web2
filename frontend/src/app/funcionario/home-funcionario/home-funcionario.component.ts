be84ed61012af552420f43d614dd09c8ff260614
DanielaTamy <danielatamyyuki@gmail.com>
2026-03-14 23:09:58 -0300

    fix: mock home funcionarios

--- frontend/src/app/funcionario/home-funcionario/home-funcionario.component.ts
+++ frontend/src/app/funcionario/home-funcionario/home-funcionario.component.ts
@@ -14,39 +14,69 @@ import { Router } from '@angular/router';
 export class HomeFuncionarioComponent {
 solicitacoes: Solicitacao[] = [
   {
+    id: 1,
     dataHoraCriacao: '2026-03-03T10:30:00',
     descricaoEquipamento: 'Notebook Dell',
     descricaoDefeito: 'Não está carregando',
     estadoAtual: SolicitacaoENUM.ABERTA,
-    cliente: { nome: 'Daniela' } as any
+    cliente: {
+      cpf: '11111111111',
+      nome: 'Daniela',
+      email: 'daniela@email.com',
+      telefone: '41999999999'
+    }
   },
   {
+    id: 2,
     dataHoraCriacao: '2026-02-06T11:10:00',
     descricaoEquipamento: 'Impressora HP',
     descricaoDefeito: 'Travando papel constantemente e imprimindo com cores erradas',
     estadoAtual: SolicitacaoENUM.ABERTA,
-    cliente: { nome: 'Laura' } as any
+    cliente: {
+      cpf: '22222222222',
+      nome: 'Laura',
+      email: 'laura@email.com',
+      telefone: '41988888888'
+    }
   },
   {
+    id: 3,
     dataHoraCriacao: '2026-03-01T12:00:00',
     descricaoEquipamento: 'Computador',
     descricaoDefeito: 'Muito lento',
     estadoAtual: SolicitacaoENUM.ABERTA,
-    cliente: { nome: 'Jess' } as any
+    cliente: {
+      cpf: '33333333333',
+      nome: 'Jess',
+      email: 'jess@email.com',
+      telefone: '41977777777'
+    }
   },
   {
+    id: 4,
     dataHoraCriacao: '2026-03-06T13:20:00',
     descricaoEquipamento: '1234567890123456789012345678901234567890',
     descricaoDefeito: 'Tela danificada',
     estadoAtual: SolicitacaoENUM.FINALIZADA,
-    cliente: { nome: 'Nathalia' } as any
+    cliente: {
+      cpf: '44444444444',
+      nome: 'Nathalia',
+      email: 'nath@email.com',
+      telefone: '41966666666'
+    }
   },
   {
+    id: 5,
     dataHoraCriacao: '2026-03-06T13:20:00',
     descricaoEquipamento: '1234567890123456789012345678901234567890',
     descricaoDefeito: 'Tela trincada',
     estadoAtual: SolicitacaoENUM.ABERTA,
-    cliente: { nome: 'Felipe' } as any
+    cliente: {
+      cpf: '55555555555',
+      nome: 'Felipe',
+      email: 'felipe@email.com',
+      telefone: '41955555555'
+    }
   }
 ];
 
@@ -59,7 +89,7 @@ get solicitacoesAbertas() {
 constructor(private router: Router) {}
 
 efetuarOrcamento(id?: number) {
-  this.router.navigate(['/funcionario/orcamento', id]);
+  this.router.navigate(['/funcionario/efetuar-orcamento', id]);
 }
 
 
