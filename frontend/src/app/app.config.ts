961e588827939fcef06c9a5ccb9ffb2b34d997cb
JessicaSouza <fatimajessica2@gmail.com>
2026-03-22 18:36:04 -0300

    realizando os ajustes solicitados

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideNativeDateAdapter } from '@angular/material/core';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideNativeDateAdapter(), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes), 
+    provideAnimationsAsync(), 
+    provideNativeDateAdapter()]
 };
