30a582f5b2188cebc3db10ea42a72039bcfb8c8f
JessicaSouza <fatimajessica2@gmail.com>
2026-04-13 16:24:20 -0300

    novas alteracoes solicitadas pela pr

--- frontend/src/app/core/models/solicitacao.model.ts
+++ frontend/src/app/core/models/solicitacao.model.ts
@@ -5,29 +5,21 @@ import { HistoricoSolicitacao } from './historico.model';
 
 export interface Solicitacao {
   id?: number;
-
   descricaoEquipamento: string;
   descricaoDefeito: string;
-
   estadoAtual: string;
-
   valorOrcado?: number;
   funcionarioOrcamento?: string; 
   dataHoraOrcamento?: string;   
-
   motivoRejeicao?: string;
   descricaoManutencao?: string;
   orientacoesCliente?: string;
-
   dataHoraCriacao: string;
   dataHoraPagamento?: string;
   dataHoraFinalizacao?: string;
-
   ativo?: boolean;
-
   cliente?: Cliente;
   categoria: CategoriaEquipamento;
   funcionarioResponsavel?: Funcionario;
-
   historico?: HistoricoSolicitacao[];
 }
